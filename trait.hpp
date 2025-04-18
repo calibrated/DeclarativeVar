2f5621deb15014c0e2e2d094261b70e6d62bac1a
KibeomYoun <kibeom.youn@gmail.com>
2025-04-18 17:38:00 +0900

    iterable 인자 추가

--- trait.hpp
+++ trait.hpp
@@ -2,6 +2,7 @@
 #define TRAIT_H
 #include <type_traits>
 #include <memory>
+#include <ranges>
 template <typename T>
 class Number;
 
@@ -30,6 +31,8 @@ template <typename D>
 struct is_shared_ptr<D, std::void_t<typename std::decay_t<D>::element_type>> : std::is_same<std::decay_t<D>, std::shared_ptr<typename std::decay_t<D>::element_type>>
 {
 };
+template <typename T>
+struct is_iterable_range : std::bool_constant<std::ranges::range<T>> {};
 
 template <typename T, typename Enable = void>
 struct ContainerTrait;
@@ -44,7 +47,7 @@ struct ContainerTrait<T, std::enable_if_t<is_shared_ptr<T>::value && is_variable
     static constexpr type value(T x) { return x; } // 그대로 반환
 };
 template <typename T>
-struct ContainerTrait<T, std::enable_if_t<!is_shared_ptr<T>::value && !is_variable<T>::value>>
+struct ContainerTrait<T, std::enable_if_t<!is_shared_ptr<T>::value && !is_variable<T>::value && !is_iterable_range<T>::value>>
 {
     using type = std::shared_ptr<Number<T>>;  // 그대로 유지
     static constexpr type value(T x) { return std::make_shared<Number<T>>(x); } // 그대로 반환
@@ -63,6 +66,24 @@ struct ContainerTrait<T, std::enable_if_t<is_variable<T>::value&& !std::is_refer
 };
 
 template <typename T>
+struct ContainerTrait<T, std::enable_if_t<is_iterable_range<T>::value,void>>
+{
+    using type = T;  //shared_ptr로 힙할당하고 수명관리
+    static constexpr type value(T x) { return x; } // 
+};
+
+template <typename T>
+struct ElementTrait<T, std::enable_if_t<is_iterable_range<T>::value,void>>
+{
+    using type = typename std::ranges::range_value_t<T>;
+    static constexpr auto eval(T list) {
+        return std::views::transform(list, [](auto const& a) {
+            
+        });
+    }
+};
+
+template <typename T>
 struct ElementTrait<T, std::enable_if_t<is_shared_ptr<T>::value &&  is_variable<typename T::element_type>::value>>
 {
     using type= typename std::decay_t<T>::element_type::variable_type;
@@ -77,7 +98,7 @@ struct ElementTrait<T, std::enable_if_t<is_variable<T>::value>>
     static constexpr type eval(T x) {return x.eval(); } 
 };
 template <typename T>
-struct ElementTrait<T, std::enable_if_t<!is_shared_ptr<T>::value && !is_variable<T>::value>>
+struct ElementTrait<T, std::enable_if_t<!is_shared_ptr<T>::value && !is_variable<T>::value && !is_iterable_range<T>::value>>
 {
     using type= T;
     static constexpr type eval(T x) {return x; } 
